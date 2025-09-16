bc7b4dcb8a335e1c3db747a953df3dbc7d10c6ee
Example User <me@example.com>
2025-09-17 01:45:52 +0900

    Fix: Add missing Progress component for build error

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab97878883034e40"; }
+/******/ 		__webpack_require__.h = function() { return "d137a00943a6602c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
